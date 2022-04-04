9a11df59373fd2f96b8c066f1608ea9611f3489f
Efornies <eleforbel@gmail.com>
2022-04-04 13:49:17 +0000

    primeros cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [selected, setSelected] = "red";
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div
+				id="red"
+				className="bg-danger"
+				onClick={() => {
+					setSelected("red");
+				}}>
+				{" "}
+			</div>
+			<div
+				id="yellow"
+				className="bg-warning"
+				onClick={() => {
+					setSelected("yellow");
+				}}>
+				{" "}
+			</div>
+			<div
+				id="green"
+				className="bg-sucess"
+				onClick={() => {
+					setSelected("green");
+				}}>
+				{" "}
+			</div>
+			{selected == "red" ? <h1> {selected} </h1> : <h1> X </h1>}
+			{selected == "red" ? <h1> {selected} </h1> : <h1> X </h1>}
+			{selected == "red" ? <h1> {selected} </h1> : <h1> X </h1>}
 		</div>
 	);
 };
